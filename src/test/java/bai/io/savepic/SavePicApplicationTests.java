66c2bfa91dcd7c400d99d6519d35239bc3bd12e4
baistan.tashkuov <baistan.xi@gmail.com>
2022-11-11 16:39:56 +0600

    Initial commit

---
+++ src/test/java/bai/io/savepic/SavePicApplicationTests.java
@@ -0,0 +1,13 @@
+package bai.io.savepic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavePicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
